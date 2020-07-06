bf28ccb6129b78dcb4a15b9c4e43ec2cacfbcaab
siler <elvlxla@gmail.com>
2020-07-07 01:21:47 +0800

    exercise1.6

--- part1/1_exercise/src/index.js
+++ part1/1_exercise/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 const Header = (props) => {
   return (
     <>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </>
   )
 }
@@ -12,42 +12,52 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part = {props.part1} exercises={props.exercises1}/>
-      <Part part = {props.part2} exercises={props.exercises2}/>
-      <Part part = {props.part3} exercises={props.exercises3}/>
+      <Part part={props.parts.parts[0]} />
+      <Part part={props.parts.parts[1]} />
+      <Part part={props.parts.parts[2]} />
     </div>
   )
 }
 const Part = (props) => {
   return (
     <p>
-      {props.part} {props.exercises}
+      {props.part.name} {props.part.exercises}
     </p>
   )
 }
 
 const Total = (props) => {
   return (
-      <div>
-        Number of exercise {props.exercises1+props.exercises2+props.exercises3}
-      </div>
+    <div>
+      Number of exercise {props.parts.parts[0].exercises + props.parts.parts[1].exercises + props.parts.parts[2].exercises}
+    </div>
   )
 }
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const part2 = 'Using props to pass data'
-  const part3 = 'State of a component'
-  const exercises1 = 10
-  const exercises2 = 7
-  const exercises3 = 14
-  return(
+  const course = {
+    name : 'Half Stack application development',
+    parts : [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  return (
     <div>
-      <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Header course={course} />
+      <Content parts={course} />
+      <Total parts={course} />
     </div>
   )
 }
---
+++ part1/6_exercise/src/index.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = ({onClick,text}) => {
+  return(
+    <button onClick={onClick}>{text}</button>
+  )
+}
+const Statistic = ({text,value}) => <div>{text}{value}</div>
+const Statistics = ({good,neutral,bad,feedbackOj}) => {
+  if(good == 0 && neutral == 0 && bad==0){
+    return(
+      <p>No feedback given</p>
+    )
+  }
+  return (
+    <div>
+      <Statistic text = {feedbackOj.good.text} value = {feedbackOj.good.value} />
+      <Statistic text = {feedbackOj.neutral.text} value = {feedbackOj.neutral.value} />
+      <Statistic text = {feedbackOj.bad.text} value = {feedbackOj.bad.value} />
+      <Statistic text = {feedbackOj.all.text} value = {feedbackOj.all.value} />
+      <Statistic text = {feedbackOj.average.text} value = {feedbackOj.average.value} />
+      <Statistic text = {feedbackOj.postitive.text} value = {feedbackOj.postitive.value} />
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const goodClick = () => setGood(good+1)
+  const neutralClick = () => setNeutral(neutral+1)
+  const badClick = () => setBad(bad+1)
+
+  const feedbackOj = {
+    good : {
+      text : "good ",
+      value : good
+    },
+    neutral : {
+      text : "neutral ",
+      value : neutral
+    },
+    bad : {
+      text : "bad ",
+      value : bad
+    },
+    all : {
+      text : "all ",
+      value : (good+neutral+bad)
+    },
+    average : {
+      text : "average ",
+      value : (((good*1)+(neutral*0)+(bad*-1))/(good+neutral+bad))
+    },
+    postitive : {
+      text : "postitive ",
+      value : ((good/(good+neutral+bad))/100 + " %")
+    }
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={goodClick} text="good"/>
+      <Button onClick={neutralClick} text="neutra"/>
+      <Button onClick={badClick} text="bad"/>
+      <h1>statistics</h1>
+      <Statistics good={good} neutral = {neutral} bad = {bad} feedbackOj = {feedbackOj}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
