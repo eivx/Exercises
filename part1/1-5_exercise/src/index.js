92c74657ecf8cf943d635ec0ca3b04f6d08e824d
siler <elvlxla@gmail.com>
2020-07-07 17:58:15 +0800

    add 1-5_exercise

--- part1/1-5_exercise/src/index.js
+++ part1/1-5_exercise/src/index.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
 const Header = (props) => {
   return (
     <>
       <h1>{props.course.name}</h1>
     </>
-  )
-}
+  );
+};
 
 const Content = (props) => {
   return (
@@ -16,50 +16,52 @@ const Content = (props) => {
       <Part part={props.parts.parts[1]} />
       <Part part={props.parts.parts[2]} />
     </div>
-  )
-}
+  );
+};
 const Part = (props) => {
   return (
     <p>
       {props.part.name} {props.part.exercises}
     </p>
-  )
-}
+  );
+};
 
 const Total = (props) => {
   return (
     <div>
-      Number of exercise {props.parts.parts[0].exercises + props.parts.parts[1].exercises + props.parts.parts[2].exercises}
+      Number of exercise{" "}
+      {props.parts.parts[0].exercises +
+        props.parts.parts[1].exercises +
+        props.parts.parts[2].exercises}
     </div>
-  )
-}
-
+  );
+};
 
 const App = () => {
   const course = {
-    name : 'Half Stack application development',
-    parts : [
+    name: "Half Stack application development",
+    parts: [
       {
-        name: 'Fundamentals of React',
-        exercises: 10
+        name: "Fundamentals of React",
+        exercises: 10,
       },
       {
-        name: 'Using props to pass data',
-        exercises: 7
+        name: "Using props to pass data",
+        exercises: 7,
       },
       {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
   return (
     <div>
       <Header course={course} />
       <Content parts={course} />
       <Total parts={course} />
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
