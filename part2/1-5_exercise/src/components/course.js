652a663856ae2bd98355296c670bcb8e50d9f243
siler <elvlxla@gmail.com>
2020-07-09 00:45:42 +0800

    add 1-5_exercise

--- part2/1-5_exercise/src/components/course.js
+++ part2/1-5_exercise/src/components/course.js
@@ -1,34 +1,34 @@
-import React from 'react'
+import React from "react";
 
 const TotalDisplay = ({ course }) => {
-    const totalArr = [];
-    for (let i = 0; i < course.parts.length; i++) {
-      totalArr.push(course.parts[i].exercises);
-    }
-    return <b>total of {totalArr.reduce((a, b) => a + b)} exercise</b>;
-  };
-  const ClassNameDisplay = ({ course }) => {
-    return <h2>{course.name}</h2>;
-  };
-  const PartDisplay = ({ course }) => {
-    return course.parts.map((note) => (
-      <p key={note.id}>
-        {note.name} {note.exercises}
-      </p>
-    ));
-  };
-  const Course = ({ course }) => {
-    return (
-      <div>
-        <h1>Web development curriculum</h1>
-        <ClassNameDisplay course={course[0]} />
-        <PartDisplay course={course[0]} />
-        <TotalDisplay course={course[0]} />
-        <ClassNameDisplay course={course[1]} />
-        <PartDisplay course={course[1]} />
-        <TotalDisplay course={course[1]} />
-      </div>
-    );
-  };
+  const totalArr = [];
+  for (let i = 0; i < course.parts.length; i++) {
+    totalArr.push(course.parts[i].exercises);
+  }
+  return <b>total of {totalArr.reduce((a, b) => a + b)} exercise</b>;
+};
+const ClassNameDisplay = ({ course }) => {
+  return <h2>{course.name}</h2>;
+};
+const PartDisplay = ({ course }) => {
+  return course.parts.map((note) => (
+    <p key={note.id}>
+      {note.name} {note.exercises}
+    </p>
+  ));
+};
+const Course = ({ course }) => {
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+      <ClassNameDisplay course={course[0]} />
+      <PartDisplay course={course[0]} />
+      <TotalDisplay course={course[0]} />
+      <ClassNameDisplay course={course[1]} />
+      <PartDisplay course={course[1]} />
+      <TotalDisplay course={course[1]} />
+    </div>
+  );
+};
 
-  export default Course
+export default Course;
